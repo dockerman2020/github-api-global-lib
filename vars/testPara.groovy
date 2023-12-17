8b1636daed4075c5b7cb8c08064bfde995f6072f
dockerman2020 <75863443+dockerman2020@users.noreply.github.com>
2023-12-17 14:57:39 +0000

    Testing param from sharedLibrary

---
+++ vars/testPara.groovy
@@ -0,0 +1,10 @@
+class testPara {
+    static void context(String mode, String environment, String database, String tableSchema) {
+        // Your logic here
+        println "Mode: $mode"
+        println "Environment: $environment"
+        println "Database: $database"
+        println "Table Schema: $tableSchema"
+        // Add your business logic or call other methods as needed
+    }
+}
